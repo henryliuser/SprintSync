9024527506b462058d2c4dfc0a7ad687b9adfb6d
Henry Liu <henryliuser@gmail.com>
2021-01-30 11:47:58 -0800

    idk

---
+++ SprintSync/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SprintSync
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasBackButton(this, false);
+            
+        }
+    }
+}
